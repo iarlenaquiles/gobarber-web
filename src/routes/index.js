3c57c2c694300f0796683c7a4ff2fbe76becbabe
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-09-24 00:50:55 -0300

    configurando rotas privadas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 
 import Signin from '../pages/Signin';
 import Signup from '../pages/Signup';
