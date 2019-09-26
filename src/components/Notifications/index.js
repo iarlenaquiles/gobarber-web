dd19d6be68a999f1acd6b6f022fb501b4a572b30
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-09-26 18:40:25 -0300

    mostrando a lista de notificação

--- src/components/Notifications/index.js
+++ src/components/Notifications/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { MdNotifications } from 'react-icons/md';
 import {
@@ -10,13 +10,18 @@ import {
 } from './styles';
 
 export default function Notifications() {
+  const [visible, setVisible] = useState(false);
+
+  function handleToggleVisible() {
+    setVisible(!visible);
+  }
   return (
     <Container>
-      <Badge hasUnread>
+      <Badge onClick={handleToggleVisible} hasUnread>
         <MdNotifications color="#7159c1" size={20} />
       </Badge>
 
-      <NotificationList>
+      <NotificationList visible={visible}>
         <Scroll>
           <Notification unread>
             <p>Você possui um novo agendamento para amanhã</p>
