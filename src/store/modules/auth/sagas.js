6ef9e23a3628f3b430bcdef303a1facdfa572747
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-09-26 02:00:09 -0300

    criando o signin

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,3 +1,29 @@
-import { all } from 'redux-saga/effects';
+import { all, takeLatest, call, put } from 'redux-saga/effects';
+import api from '~/services/api';
+import history from '~/services/history';
+import { signInSuccess } from './actions';
 
-export default all([]);
+export function* signIn({ payload }) {
+  const { email, password } = payload;
+  try {
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
+    console.tron.log(response);
+    const { token, user } = response.data;
+
+    if (!user.provider) {
+      console.tron.log('usuário nao é um prestador');
+      return;
+    }
+
+    yield put(signInSuccess(token, user));
+
+    history.push('/dashboard');
+  } catch (error) {
+    console.tron.log(error);
+  }
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
