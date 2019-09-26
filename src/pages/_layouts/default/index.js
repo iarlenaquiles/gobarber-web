84cfddbfbf523ea809b00a722877a49f51e50d62
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-09-26 16:36:25 -0300

    criando header

--- src/pages/_layouts/default/index.js
+++ src/pages/_layouts/default/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import PropType from 'prop-types';
+
+import Header from '~/components/Header';
 import { Wrapper } from './styles';
 
 export default function DefaultLayout({ children }) {
-  return <Wrapper>{children}</Wrapper>;
+  return (
+    <Wrapper>
+      <Header />
+      {children}
+    </Wrapper>
+  );
 }
 
 DefaultLayout.propType = {
