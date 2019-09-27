10877c1f57431c93d62d30639fe8960fb3954cf3
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-09-26 23:35:03 -0300

    estilizando a dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import api from '~/services/api';
 
-// import { Container } from './styles';
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  return <h1>Dash</h1>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft color="#fff" size={36} />
+        </button>
+        <strong>31 de Maio</strong>
+        <button type="button">
+          <MdChevronRight color="#fff" size={36} />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Iarlen</span>
+        </Time>
+
+        <Time>
+          <strong>09:00</strong>
+          <span>Em abert</span>
+        </Time>
+
+        <Time>
+          <strong>10:00</strong>
+          <span>Iarlen</span>
+        </Time>
+
+        <Time>
+          <strong>11:00</strong>
+          <span>Iarlen</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
