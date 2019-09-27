f4fa848e2579ade8efa37f54f8daa04aa680959e
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-09-26 22:20:47 -0300

    criando o saga do profile

--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -1,6 +1,26 @@
 import { all, takeLatest, call, put } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 import api from '~/services/api';
-import history from '~/services/history';
-import { signInSuccess } from './actions';
+import { updateProfileSuccess, updateProfileFailure } from './actions';
 
-export default all([]);
+export function* updateProfile({ payload }) {
+  try {
+    const { name, email, ...rest } = payload.data;
+
+    const profile = {
+      name,
+      email,
+      ...(rest.oldPassword ? rest : {}),
+    };
+
+    const response = yield call(api.put, 'users', profile);
+
+    toast.success('Perfil atualizado com sucesso!');
+    yield put(updateProfileSuccess(response.data));
+  } catch (error) {
+    toast.error('Erro ao atualizar perfil, confira seus dados');
+    yield put(updateProfileFailure());
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
