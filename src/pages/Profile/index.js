494cc21b047b8aaa9d11a54dd80d497529fda039
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-09-26 23:08:46 -0300

    fazendo o logout da aplicação

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -5,6 +5,7 @@ import { Form, Input } from '@rocketseat/unform';
 import { Container } from './styles';
 import AvatarInput from './AvatarInput';
 import { updateProfileRequest } from '~/store/modules/user/actions';
+import { signOut } from '~/store/modules/auth/actions';
 
 export default function Profile() {
   const profile = useSelector(state => state.user.profile);
@@ -13,6 +14,10 @@ export default function Profile() {
 
   function handleSubmit(data) {
     dispatch(updateProfileRequest(data));
+  }
+
+  function handleSignOut() {
+    dispatch(signOut());
   }
 
   return (
@@ -39,7 +44,9 @@ export default function Profile() {
         <button type="submit">Atualizar perfil</button>
       </Form>
 
-      <button type="submit">Sair do GoBarber</button>
+      <button type="submit" onClick={handleSignOut}>
+        Sair do GoBarber
+      </button>
     </Container>
   );
 }
